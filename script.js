162108d7b89200988b2e70f58bad13e90d84b9e9
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-20 10:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-let counter = 0;
+/*let counter = 0;
 document.getElementById("counter").textContent = counter;
 
 document.getElementById("incrementBtn").addEventListener("click", function() {
     alert("Current Counter Value: " + counter);
     counter++;
     document.getElementById("counter").textContent = counter;
-});
+});*/
+() => { 
+  cy.visit(baseUrl); 
+  cy.on('window:alert', alertText => { 
+    const counterValue = parseInt(alertText.replace('Current Counter Value: ', ''));
+    expect(counterValue).to.equal(0); 
+  }); 
+  cy.get('#incrementBtn').click(); 
+}
+
 
