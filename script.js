7e58796cfdfbe8907a5605f917ed4ad98ea23446
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-20 09:48:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let counter = 0;
+document.getElementById("counter").innerHTML = counter;
+
+document.getElementById("incrementBtn").addEventListener("click", function() {
+    alert("Current Counter Value: " + counter);
+    counter++;
+    document.getElementById("counter").innerHTML = counter;
+});
