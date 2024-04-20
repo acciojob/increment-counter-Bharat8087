5f963a112e11e523b312689e6ee2ca0545930f7d
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-20 10:15:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-/*let counter = 0;
-document.getElementById("counter").textContent = counter;
+describe("Increment Counter Tests", () => {
+  it("should increment counter and display correct initial value", () => { 
+    cy.visit(baseUrl); 
+    cy.on('window:alert', alertText => { 
+      const counterValue = parseInt(alertText.replace('Current Counter Value: ', ''));
+      expect(counterValue).to.equal(0); 
+    }); 
+    cy.get('#incrementBtn').click(); 
+    cy.get('#counter').should('have.text', '1'); 
+  });
 
-document.getElementById("incrementBtn").addEventListener("click", function() {
-    alert("Current Counter Value: " + counter);
-    counter++;
-    document.getElementById("counter").textContent = counter;
-});*/
-() => { 
-  cy.visit(baseUrl); 
-  cy.on('window:alert', alertText => { 
-    const counterValue = parseInt(alertText.replace('Current Counter Value: ', ''));
-    expect(counterValue).to.equal(0); 
-  }); 
-  cy.get('#incrementBtn').click(); 
-	  cy.get('#counter').should('have.text', '1'); 
-
-}() => { 
-  cy.visit(baseUrl); 
-  cy.get('#incrementBtn').click(); 
-  cy.get('#counter').should('have.text', '1'); 
-  cy.get('#incrementBtn').click(); 
-  cy.get('#counter').should('have.text', '2'); 
-  cy.get('#incrementBtn').click(); 
-  cy.get('#counter').should('have.text', '3'); 
-}
-
-
-
+  it("should increment counter correctly multiple times", () => { 
+    cy.visit(baseUrl); 
+    cy.get('#incrementBtn').click(); 
+    cy.get('#counter').should('have.text', '1'); 
+    cy.get('#incrementBtn').click(); 
+    cy.get('#counter').should('have.text', '2'); 
+    cy.get('#incrementBtn').click(); 
+    cy.get('#counter').should('have.text', '3'); 
+  });
+});
