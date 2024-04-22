1cfb1370eb0df94f2138305fb72d59b0f2c2a39f
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-22 08:38:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let counter = 0;
+/*let counter = 0;
 document.getElementById("counter").textContent = counter;
 
 document.getElementById("incrementBtn").addEventListener("click", function() {
     alert(" " + counter);
     counter++;
     document.getElementById("counter").textContent = counter;
-});
+});*/
 
 
